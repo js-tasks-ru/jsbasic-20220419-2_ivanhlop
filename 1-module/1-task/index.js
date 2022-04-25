06550b52e830b24829ad83b697e1e34271f1376a
Ivan Khlopachev <ivanhlop@gmail.com>
2022-04-25 15:34:11 +0300

    1-module\1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+  let result=1;
+    for (n;n>=0;n--){
+        if (n>0){
+        n;
+        }
+        else if (n=0){
+        n==n+1;
+        }
+        else {
+        break;
+    }
+    result *= n;
+} 
+return result;
 }
